787795fb4177917c04ced1c0fdd0f279fb1cf19a
João Figueiredo <joao.ggwp@outlook.com>
2025-07-13 08:48:00 -0300

    test: ensure SignUpController returns 400 if password confrimation fails

--- tests/presentation/controllers/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -87,6 +87,23 @@ describe("SignUpController", () => {
     );
   });
 
+  test("Should return 400 if password confirmation does not match", () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "wrong_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError("passwordConfirmation")
+    );
+  });
+
   test("Should return 400 if an invalid email is provided", () => {
     const { sut, emailValidatorStub } = makeSut();
 
