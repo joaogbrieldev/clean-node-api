562a31ee86023ac4ddec2a5b990d2a41bc7f85c4
João Figueiredo <joao.ggwp@outlook.com>
2025-07-14 08:37:34 -0300

    test: ensure EmailValidatorAdapter returns true

--- tests/presentation/utils/email-validator-adapter.spec.ts
+++ tests/presentation/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,17 @@
 import { EmailValidatorAdapter } from "@/utils/email-validator";
+import validator from "validator";
 
 describe("EmailValidator Adapter", () => {
   test("Should return false if validator returns false", () => {
     const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
+  });
+  test("Should return true if validator returns true", () => {
+    const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(true);
+    const isValid = sut.isValid("valid_email@mail.com");
+    expect(isValid).toBe(true);
   });
 });
