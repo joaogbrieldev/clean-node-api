311e16bb50d72b9725b2867e13b45ee9ea52703c
João Figueiredo <joao.ggwp@outlook.com>
2025-07-14 07:41:07 -0300

    test: test husky

--- tests/presentation/controllers/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -227,7 +227,7 @@ describe("SignUpController", () => {
       },
     };
     const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(201);
+    expect(httpResponse.statusCode).toBe(200);
     expect(httpResponse.body).toEqual({
       id: "valid_id",
       name: "valid_name",
