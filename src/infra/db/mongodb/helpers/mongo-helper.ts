16d4cfe8be3f1970379107b0fd49939373fcabab
João Figueiredo <joao.ggwp@outlook.com>
2025-07-17 19:27:06 -0300

    feat: ensure AccountMongoRepository returns an account on sucess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { Collection, MongoClient } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -7,5 +7,12 @@ export const MongoHelper = {
   },
   async disconnect(): Promise<void> {
     await this.client.close();
+  },
+  async getCollection(name: string): Promise<Collection> {
+    return this.client.db().collection(name);
+  },
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id.toHexString() });
   },
 };
