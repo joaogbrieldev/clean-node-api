3baa93c0178684eede942ca49ab9694e333ce6d2
João Figueiredo <joao.ggwp@outlook.com>
2025-07-17 07:53:44 -0300

    test: ensure BcryptAdapter returns a hash on sucess

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,7 +2,9 @@ import { BcryptAdapter } from "@/infra/criptography/bcrypt-adapter";
 import bcrypt from "bcrypt";
 
 jest.mock("bcrypt", () => ({
-  hash: jest.fn(),
+  async hash(value: string, salt: number): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
 }));
 
 describe("Bcrypt Adapter", () => {
@@ -12,5 +14,11 @@ describe("Bcrypt Adapter", () => {
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+  test("Should return a hash on success", async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hash");
   });
 });
