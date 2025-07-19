3300a29df5ae64fec33d3253725ad9306bb4401f
João Figueiredo <joao.ggwp@outlook.com>
2025-07-19 08:32:25 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,9 @@
+import { MongoHelper } from "@/infra/db/mongodb/helpers/mongo-helper";
 import app from "./config/app";
+import env from "./config/env";
 
-app.listen(5050, () => {
-  console.log("Server is running on port 5050");
+MongoHelper.connect(env.mongoUrl).then(() => {
+  app.listen(env.port, () => {
+    console.log(`Server is running on port ${env.port}`);
+  });
 });
