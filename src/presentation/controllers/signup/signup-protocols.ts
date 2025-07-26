c80b2fba5ab14de0567ec214dfd1dd10bf9e21a2
João Figueiredo <joao.ggwp@outlook.com>
2025-07-25 22:32:02 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,3 +1,4 @@
 export * from "@/domain/usecases/add-account";
+export * from "@/presentation/helpers/validators/validation";
 export * from "@/presentation/protocols";
 export * from "@/presentation/protocols/email-validator";
