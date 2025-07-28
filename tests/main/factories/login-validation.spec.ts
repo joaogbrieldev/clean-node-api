0f9c1b1c32967648a3f3503f6e25dc2b05da3edc
João Figueiredo <joao.ggwp@outlook.com>
2025-07-28 07:31:24 -0300

    refactor: group validation imports in a single file

--- tests/main/factories/login-validation.spec.ts
+++ tests/main/factories/login-validation.spec.ts
@@ -1,7 +1,9 @@
 import { makeLoginValidation } from "@/main/factories/login/login-validation";
-import { EmailValidation } from "@/presentation/helpers/validators/email-validation";
-import { RequiredFieldValidation } from "@/presentation/helpers/validators/required-field-validation";
-import { ValidationComposite } from "@/presentation/helpers/validators/validation-composite";
+import {
+  EmailValidation,
+  RequiredFieldValidation,
+  ValidationComposite,
+} from "@/presentation/helpers/validators";
 import { EmailValidator } from "@/presentation/protocols/email-validator";
 import { Validation } from "@/presentation/protocols/validation";
 
