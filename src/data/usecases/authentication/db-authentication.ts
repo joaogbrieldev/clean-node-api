b42974365639e068341fa500d9d9f38aa256a745
João Figueiredo <joao.ggwp@outlook.com>
2025-07-30 23:06:48 -0300

    feat: ensure DbAuthentication calls repository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,6 +1,7 @@
 import { HashComparer } from "@/data/protocols/criptography/hash-comparer";
 import { TokenGenerator } from "@/data/protocols/criptography/token-generator";
 import { LoadAccountByEmailRepository } from "@/data/protocols/db/load-account-by-email-repository";
+import { UpdateAccessTokenRepository } from "@/data/protocols/db/update-access-token-repository";
 import {
   Authentication,
   AuthenticationModel,
@@ -10,7 +11,8 @@ export class DBAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly tokenGenerator: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
   async auth(authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.load(
@@ -23,6 +25,7 @@ export class DBAuthentication implements Authentication {
       );
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id);
+        await this.updateAccessTokenRepository.update(account.id, accessToken);
         return accessToken;
       }
     }
