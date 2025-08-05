2b2160feae2708f417fd72faf236f8f85938811b
João Figueiredo <joao.ggwp@outlook.com>
2025-08-04 23:30:16 -0300

    feat: add Login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
+import { makeLoginController } from "@/main/factories/login/login-factory";
 import { makeSignUpController } from "@/main/factories/signup";
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
 
 export default (router: Router): void => {
   router.post("/signup", adaptRoute(makeSignUpController()));
+  router.post("/login", adaptRoute(makeLoginController()));
 };
