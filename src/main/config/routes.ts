1a60a3bed814a362dd3e723c4f4196baca324692
João Figueiredo <joao.ggwp@outlook.com>
2025-08-17 16:22:11 -0300

    feat: add AddSurvey composition

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,10 @@
 import loginRoutes from "@/main/routes/login-routes";
+import surveyRoutes from "@/main/routes/survey-routes";
 import { Express, Router } from "express";
 
 export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
   loginRoutes(router);
+  surveyRoutes(router);
 };
