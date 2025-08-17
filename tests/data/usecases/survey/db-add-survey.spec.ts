c6ecf267fec6f999dfc1d3cb80bb6cbb1358195c
João Figueiredo <joao.ggwp@outlook.com>
2025-08-17 10:24:28 -0300

    refactor

--- tests/data/usecases/survey/db-add-survey.spec.ts
+++ tests/data/usecases/survey/db-add-survey.spec.ts
@@ -2,6 +2,26 @@ import { AddSurveyRepository } from "@/data/protocols/db/survey/add-survey-repos
 import { DbAddSurvey } from "@/data/usecases/add-survey/db-add-survey";
 import { AddSurveyModel } from "@/domain/usecases/add-survey";
 
+type SutTypes = {
+  sut: DbAddSurvey;
+  addSurveyRepositoryStub: AddSurveyRepository;
+};
+
+class AddSurveyRepositoryStub implements AddSurveyRepository {
+  async add(surveyData: AddSurveyModel): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = new AddSurveyRepositoryStub();
+  const sut = new DbAddSurvey(addSurveyRepositoryStub);
+  return {
+    sut,
+    addSurveyRepositoryStub,
+  };
+};
+
 describe("DbAddSurvey UseCase", () => {
   const makeFakeSurvey = (): AddSurveyModel => ({
     question: "any_question",
@@ -13,15 +33,8 @@ describe("DbAddSurvey UseCase", () => {
     ],
   });
 
-  class AddSurveyRepositoryStub implements AddSurveyRepository {
-    async add(surveyData: AddSurveyModel): Promise<void> {
-      return Promise.resolve();
-    }
-  }
-
   test("should call AddSurveyRepository with correct values", async () => {
-    const addSurveyRepositoryStub = new AddSurveyRepositoryStub();
-    const sut = new DbAddSurvey(addSurveyRepositoryStub);
+    const { sut, addSurveyRepositoryStub } = makeSut();
     const addSurveySpy = jest.spyOn(addSurveyRepositoryStub, "add");
     const surveyData = makeFakeSurvey();
     await sut.add(surveyData);
