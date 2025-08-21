c5bda4f9318ddf474cf6eeb039d989a07bcc7848
João Figueiredo <joao.ggwp@outlook.com>
2025-08-21 20:47:15 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -41,5 +41,10 @@ describe("Jwt Adapter", () => {
       await sut.decrypt("any_token");
       expect(verifySpy).toHaveBeenCalledWith("any_token", "secret");
     });
+    test("Should return a value if verify succeeds", async () => {
+      const sut = makeSut();
+      const value = await sut.decrypt("any_token");
+      expect(value).toBe("any_value");
+    });
   });
 });
