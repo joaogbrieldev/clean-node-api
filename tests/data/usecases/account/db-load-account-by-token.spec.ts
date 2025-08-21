1ffbc08886f93b0f4380d8ee14f1378b7e5421e2
João Figueiredo <joao.ggwp@outlook.com>
2025-08-21 19:39:27 -0300

    test: create LoadAccountByToken interface

--- tests/data/usecases/account/db-load-account-by-token.spec.ts
+++ tests/data/usecases/account/db-load-account-by-token.spec.ts
@@ -1,8 +1,9 @@
 import { Decrypter } from "@/data/protocols/criptography/decrypter";
+import { LoadAccountByTokenRepository } from "@/data/protocols/db/load-account-by-token";
 import { DbLoadAccountByToken } from "@/data/usecases/load-account-by-token/db-load-account-by-token";
 
 type SutTypes = {
-  sut: DbLoadAccountByToken;
+  sut: LoadAccountByTokenRepository;
   decrupterStub: Decrypter;
 };
 
@@ -25,7 +26,13 @@ describe("DbLoadAccountByToken", () => {
   test("should call Decrypter with correct access token", async () => {
     const { sut, decrupterStub } = makeSut();
     const decryptSpy = jest.spyOn(decrupterStub, "decrypt");
-    await sut.load("any_token");
+    await sut.load("any_token", "any_role");
     expect(decryptSpy).toHaveBeenCalledWith("any_token");
+  });
+  test("should return null if Decrypter returns null", async () => {
+    const { sut } = makeSut();
+    jest.spyOn(sut, "load").mockReturnValueOnce(Promise.resolve(null));
+    const account = await sut.load("any_token", "any_role");
+    expect(account).toBeNull();
   });
 });
