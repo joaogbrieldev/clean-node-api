bccc3def6c0e30a1572820dbcf24e5fe12720366
João Figueiredo <joao.ggwp@outlook.com>
2025-08-26 21:39:36 -0300

    feat: ensure LoadSurveysController throws if loadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,12 +1,16 @@
 import { LoadSurveys } from "@/domain/usecases/load-surveys";
-import { ok } from "@/presentation/helpers/http/http-helper";
+import { ok, serverError } from "@/presentation/helpers/http/http-helper";
 import { Controller } from "@/presentation/protocols";
 import { HttpRequest, HttpResponse } from "@/presentation/protocols/http";
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load();
-    return ok(surveys);
+    try {
+      const surveys = await this.loadSurveys.load();
+      return ok(surveys);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
