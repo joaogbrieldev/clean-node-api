dab24617f1ad29015da0790a7e9bdb88ec84b01a
João Figueiredo <joao.ggwp@outlook.com>
2025-08-26 23:32:53 -0300

    test: ensure SurveyMongoRepository loadAll on sucess

--- tests/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -21,28 +21,51 @@ describe("Account Mongo Repository", () => {
     return new SurveyMongoRepository();
   };
 
-  test("Should return an survey on add survey success", async () => {
-    const sut = makeSut();
-    await sut.add({
-      question: "any_question",
-      answers: [
+  describe("Add", () => {
+    test("Should return an survey on add survey success", async () => {
+      const sut = makeSut();
+      await sut.add({
+        question: "any_question",
+        answers: [
+          {
+            answer: "any_answer",
+            image: "any_image",
+          },
+        ],
+        date: new Date(),
+      });
+      const surveyInDb = await surveyCollection.findOne({
+        question: "any_question",
+      });
+      expect(surveyInDb).toBeTruthy();
+      expect(surveyInDb.question).toBe("any_question");
+      expect(surveyInDb.answers).toEqual([
         {
           answer: "any_answer",
           image: "any_image",
         },
-      ],
-      date: new Date(),
+      ]);
     });
-    const surveyInDb = await surveyCollection.findOne({
-      question: "any_question",
+  });
+  describe("LoadAll", () => {
+    test("Should return all surveys on success", async () => {
+      const sut = makeSut();
+      await surveyCollection.insertMany([
+        {
+          question: "any_question",
+          answers: [{ answer: "any_answer" }],
+          date: new Date(),
+        },
+        {
+          question: "other_question",
+          answers: [{ answer: "other_answer" }],
+          date: new Date(),
+        },
+      ]);
+      const surveys = await sut.loadAll();
+      expect(surveys.length).toBe(2);
+      expect(surveys[0].question).toBe("any_question");
+      expect(surveys[1].question).toBe("other_question");
     });
-    expect(surveyInDb).toBeTruthy();
-    expect(surveyInDb.question).toBe("any_question");
-    expect(surveyInDb.answers).toEqual([
-      {
-        answer: "any_answer",
-        image: "any_image",
-      },
-    ]);
   });
 });
